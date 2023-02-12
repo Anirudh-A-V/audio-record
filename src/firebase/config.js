0de84ae7117ed500755b3ebd2d00589f93bf1817
Anirudh A V <anirudh.av02@gmail.com>
2023-02-12 08:37:28 +0530

    feat: firebase app initialised

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5J9TBDCva_56KWLWcOSxGL6KbqVYkMmk",
+  authDomain: "audio-recorder-2002911.firebaseapp.com",
+  projectId: "audio-recorder-2002911",
+  storageBucket: "audio-recorder-2002911.appspot.com",
+  messagingSenderId: "719837226838",
+  appId: "1:719837226838:web:722c9473caf94d43cb58f7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
