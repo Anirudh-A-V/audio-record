f4209c38849f217a37cf58ef83eb956f6da9c74c
Anirudh A V <anirudh.av02@gmail.com>
2023-02-12 14:14:08 +0530

    feat: useFirestore hook created

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { db } from "../firebase/config";
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
+
+const useFirestore = (collections) => {
+    const [docs, setDocs] = useState([]);
+
+    useEffect(() => {
+        const collectionRef = collection(db, collections);
+        const q = query(collectionRef, orderBy('createdAt', 'desc'));
+            
+            const unsubscribe = onSnapshot(q, (snap) => {
+                let documents = [];
+                snap.forEach(doc => {
+                    documents.push({ ...doc.data(), id: doc.id });
+                });
+                setDocs(documents);
+            });
+        
+        return () => unsubscribe();
+
+    }, [collections]);
+
+    return { docs };
+}
+
+export default useFirestore;
